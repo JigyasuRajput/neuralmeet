00a2424120d73cb59609d25093ae2140700bd38f
Jigyasu Rajput <jigyasu2021@gmail.com>
2025-08-01 18:23:40 +0530

    completed state WIP

---
+++ src/modules/meetings/ui/components/completed-state.tsx
@@ -0,0 +1,94 @@
+import Markdown from "react-markdown";
+import link from "next/link";
+import {
+  SparklesIcon,
+  FileTextIcon,
+  BookOpenTextIcon,
+  FileVideoIcon,
+  ClockFadingIcon,
+} from "lucide-react";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { MeetingGetOne } from "../../types";
+
+interface Props {
+    data: MeetingGetOne;
+}
+
+export const CompletedState = ({ data }: Props) => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <Tabs defaultValue="summary">
+        <div className="bg-white rounded-lg border px-3">
+          <ScrollArea>
+            <TabsList className="p-0 bg-background justify-start rounded-none h-13">
+              <TabsTrigger 
+                value="summary"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <BookOpenTextIcon />
+                Summary
+              </TabsTrigger>
+
+              <TabsTrigger 
+                value="transcript"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <FileTextIcon />
+                Transcript
+              </TabsTrigger>
+              
+              <TabsTrigger 
+                value="recording"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <FileVideoIcon />
+                Recording
+              </TabsTrigger>
+              
+              <TabsTrigger 
+                value="chat"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <SparklesIcon />
+                Ask AI
+              </TabsTrigger>
+            </TabsList>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </div>
+        <TabsContent value="recording">
+          <div className="bg-white rounded-lg border px-4 py-5">
+            <video
+              src={data.recordingUrl!}
+              className="w-full rounded-lg"
+              controls
+            />
+          </div>
+        </TabsContent>
+
+        <TabsContent value="summary">
+          <div className="bg-white rounded-lg border">
+            <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+              <h2 className="text-2xl font-medium capitalize">{data.name}</h2>
+              <div className="flex gap-x-2 items-center">
+                <Link
+                  href={`/agents/${data.agent.id}`}
+                  className="flex items-center gap-x-2 underline underline-offset-4 capitalize"
+                >
+                  <GeneratedAvatar
+                    variant="botttsNeutral"
+                    seed={data.agent.name}
+                    className="size-5"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </TabsContent>
+
+      </Tabs>
+    </div>
+  );
+};
